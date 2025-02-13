35544750b8f5d85d1acab964fc9d6be04cdc40d4
Xin <2457803917@qq.com>
2025-02-13 10:11:06 +0800

    vs

--- script/main.py
+++ script/main.py
@@ -6,8 +6,12 @@
 
 import os
 import time
+import sys
 os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+    print("已将项目根目录添加到 sys.path")
 import config as cf
 from model_file import creat_model
 from train_process import model_train
